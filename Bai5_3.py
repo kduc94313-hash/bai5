a137d6af50fa7dbd312a9b2cc6b1c63bbbc7521f
kduc94313-hash <kduc94313@gmail.com>
2025-12-02 23:45:48 +0700

    Create Bai5_3.py

---
+++ Bai5_3.py
@@ -0,0 +1,8 @@
+print("Sinh vien: Dau Van Khanh Duc")
+print("Ma so SV : 245752021610021")
+print("###########################")
+#################################
+import numpy as np
+x = np.arange(12, 38)
+
+print(x)
